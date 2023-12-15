1c8b9a3e08ba6710c7c954e6926759a05a816bf7
RimiSkiller <54591781+RimiSkiller@users.noreply.github.com>
2023-12-16 01:14:29 +0300

    added gpt to welcome

--- src/events/guildMemberAdd/welcomer.js
+++ src/events/guildMemberAdd/welcomer.js
@@ -1,12 +1,14 @@
 const { createCanvas, registerFont, loadImage } = require('canvas');
 const { welcome } = require('../../../configs/config.json');
+const { OpenAI } = require('openai');
+const openai = new OpenAI({ apiKey: process.env.OPEN_AI });
 
 /**
  * @param {import('discord.js').Client} client
  * @param {import('discord.js').GuildMember} member
  */
 module.exports = async (client, member) => {
-	if (member.guild.id != client.mainServer.id) return;
+	// if (member.guild.id != client.mainServer.id) return;
 	const canvas = createCanvas(1920, 1080);
 	const ctx = canvas.getContext('2d');
 
@@ -26,7 +28,25 @@ module.exports = async (client, member) => {
 		ctx.clip();
 		ctx.drawImage(image, 115, 240, 630, 630);
 	});
-	const messages = await client.staffServer.channels.cache.get(welcome.messages).messages.fetch();
-	const message = messages.map(msg => msg.content.replace('{member}', `<@${member.id}>`))[Math.floor(Math.random() * messages.size)];
-	client.mainServer.channels.cache.get(welcome.channel).send({ content: message, files: [canvas.toBuffer()] });
+
+
+	openai.chat.completions.create({
+		model: 'gpt-3.5-turbo-16k-0613',
+		messages: [
+			{
+				role: 'user',
+				content: `as a Discord user, send a single  short welcome message to a user named "${member.displayName}" who joined a server named "${member.guild.name}", make the message related to the user name.`,
+			},
+		],
+	})
+		.then(response => {
+			let answer = response.choices[0].message.content;
+			answer = answer.slice(1, answer.length - 1);
+			client.mainServer.channels.cache.get(welcome.channel).send({ content: '**● ' + answer.replace(member.displayName, `<@${member.id}>`) + '**', files: [canvas.toBuffer()] });
+		});
+
+
+	// const messages = await client.staffServer.channels.cache.get(welcome.messages).messages.fetch();
+	// const message = messages.map(msg => msg.content.replace('{member}', `<@${member.id}>`))[Math.floor(Math.random() * messages.size)];
+	// client.mainServer.channels.cache.get(welcome.channel).send({ content: message, files: [canvas.toBuffer()] });
 };
